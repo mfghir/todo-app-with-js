e5815e795e8e2c92c83cf0cbb419ced66fd03ab2
miss-f-g <fatemeghafari98@gmail.com>
2022-07-14 11:23:49 +0430

    11.write removeLocalTodos func

--- app.js
+++ app.js
@@ -128,3 +128,15 @@ function getTodos() {
   });
 }
 
+function removeLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const todoIndex = todo.children[0].innerText;
+  todos.splice(todos.indexOf(todoIndex), 1);
+  localStorage.setItem("todo", JSON.stringify(todos));
+}
