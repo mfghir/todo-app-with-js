2615b74a463966861cc8e7e9292bbb4525b4b047
miss-f-g <fatemeghafari98@gmail.com>
2022-07-14 11:19:21 +0430

    9.write saveLocalTodos func

--- app.js
+++ app.js
@@ -82,3 +82,15 @@ function filterTodo(e) {
   });
 }
 
+function saveLocalTodos(todo) {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
