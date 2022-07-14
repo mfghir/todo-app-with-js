cad2802ee890057ab6b2cfe9fc0185007ca54ebc
miss-f-g <fatemeghafari98@gmail.com>
2022-07-14 11:22:59 +0430

    10.write getTodos func

--- app.js
+++ app.js
@@ -94,3 +94,37 @@ function saveLocalTodos(todo) {
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
+function getTodos() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  todos.forEach((todo) => {
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    const todoLi = document.createElement("li");
+    todoLi.innerText = todo;
+    todoLi.classList.add("todo-item");
+
+    todoDiv.appendChild(todoLi);
+    todoInput.value = "";
+
+    const completedBtn = document.createElement("button");
+    completedBtn.innerHTML = "<i class='fa fa-check' aria-hidden='true'></i>";
+    completedBtn.classList.add("completedBtn");
+
+    todoDiv.appendChild(completedBtn);
+
+    const trashBtn = document.createElement("button");
+    trashBtn.innerHTML = "<i class='fa fa-trash' aria-hidden='true'></i>";
+    trashBtn.classList.add("trashBtn");
+
+    todoDiv.appendChild(trashBtn);
+    todoList.appendChild(todoDiv);
+  });
+}
+
